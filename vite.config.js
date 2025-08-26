1cccc3633e1da14889bd445ce0299f20f5226b82
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2025-08-26 13:10:57 +0200

    fix(abel): add envirotment vars

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-const base = process.env.BASE_URL || '/';
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: base,
+  base: process.env.VITE_BASE_URL || '/',
   server: {
     port: 3000, // <-- aquí defines el puerto
   },
