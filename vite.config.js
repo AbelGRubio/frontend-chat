d90c6cd6774dbee22c2021bf5328b8e7b8430aa1
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2025-08-26 12:05:32 +0200

    fix(abel): change vite configuration and docs. Added deploy actions in github pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const base = process.env.BASE_URL || '/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: base,
   server: {
     port: 3000, // <-- aquí defines el puerto
   },
